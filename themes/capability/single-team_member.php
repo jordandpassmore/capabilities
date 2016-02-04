8115e82e348e13071c58bf81b912208ebf371458
Jordan <jordan@millenniumgd.com>
2016-02-04 17:21:36 -0500

    First Changes to Team Member template

--- themes/capability/single-team_member.php
+++ themes/capability/single-team_member.php
@@ -11,19 +11,25 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+                    
 		<?php
 		while ( have_posts() ) : the_post();
 
-			get_template_part( 'template-parts/content', get_post_format() );
-
-			the_post_navigation();
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
+			get_template_part( 'template-parts/content', get_post_format() ); ?>
+                        <div class="team-member-details">
+                                <h3>Team Member Details</h3>
+                                <p><strong>Email Address: </strong><?php the_field('email_address'); ?></p>
+                                <p><strong>Phone Number: </strong><?php the_field('phone_number'); ?></p>
+                                <p><strong>Extension: </strong><?php the_field('extension'); ?></p>
+                                <p><strong>Service Since: </strong><?php the_field('service_since'); ?></p>
+                        </div>
+                        <div class="social-media">
+                                <h3>Social Media</h3>
+                                <p><strong>Linked In: </strong><?php the_field('linkedin'); ?></p>
+                                <p><strong>Twitter: </strong><?php the_field('twitter'); ?></p>
+                                <p><strong>Instagram: </strong><?php the_field('instagram'); ?></p>
+                        </div>
+                <?php endwhile; // End of the loop.
 		?>
 
 		</main><!-- #main -->
