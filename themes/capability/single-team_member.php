dc4e721d1c45c78f43762f5a3dc70f27289d86bf
Jordan Passmore <jordandpassmore@gmail.com>
2016-02-05 01:22:21 -0500

    Worked on Single Team Member Page

--- themes/capability/single-team_member.php
+++ themes/capability/single-team_member.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all single posts.
+ * The template for displaying single team members.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
@@ -11,25 +11,13 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-                    
+
 		<?php
 		while ( have_posts() ) : the_post();
 
-			get_template_part( 'template-parts/content', get_post_format() ); ?>
-                        <div class="team-member-details">
-                                <h3>Team Member Details</h3>
-                                <p><strong>Email Address: </strong><?php the_field('email_address'); ?></p>
-                                <p><strong>Phone Number: </strong><?php the_field('phone_number'); ?></p>
-                                <p><strong>Extension: </strong><?php the_field('extension'); ?></p>
-                                <p><strong>Service Since: </strong><?php the_field('service_since'); ?></p>
-                        </div>
-                        <div class="social-media">
-                                <h3>Social Media</h3>
-                                <p><strong>Linked In: </strong><?php the_field('linkedin'); ?></p>
-                                <p><strong>Twitter: </strong><?php the_field('twitter'); ?></p>
-                                <p><strong>Instagram: </strong><?php the_field('instagram'); ?></p>
-                        </div>
-                <?php endwhile; // End of the loop.
+			get_template_part( 'template-parts/content-team_members', get_post_format() );
+
+		endwhile; // End of the loop.
 		?>
 
 		</main><!-- #main -->
---
+++ themes/capability/template-parts/content-team_members.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Template part for displaying a Team Member
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Capablity
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header axt">
+		<?php
+			if ( is_single() ) { ?>
+                            <div class="team-member-page-heading">
+                            <?php if ( has_post_thumbnail() ) {
+                                the_post_thumbnail();
+                            } else {
+                                $placeholder_image = wp_get_attachment_image('1741', 'full');
+                                echo $placeholder_image;
+                            }
+                            the_title( '<h1 class="team-member-name">', '</h1>' ) ?>
+                            </div>
+                            <?php
+			} else {
+				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			}
+
+		if ( 'post' === get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php capability_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		<?php
+		endif; ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+            <div class="team-member-contact">
+                <ul class="contact-list">
+                    <li><i class="fa fa-calendar"></i>Service since <?php the_field('service_since') ?></li>
+                    <li><a href="mailto: <?php the_field('email_address') ?>"><i class="fa fa-envelope"></i>Send a message</a></li>
+                    <li><i class="fa fa-phone"></i><?php the_field('phone_number') ?> Ext: <?php the_field('extension') ?></li>
+                </ul>
+                
+            </div>
+            <div class="q-and-a">
+                <?php 
+                $all_fields = get_field_objects();
+                foreach ($all_fields as $afield) {
+                    $two_letters = substr($afield['name'], 0, 2);
+                    if ($two_letters == "tq" && $afield['value']) { ?>
+                    <h4><?php echo $afield['label'] ?></h4>
+                    <p><?php echo $afield['value'] ?></p>
+                    <?php }
+                }
+                ?>
+            </div>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php capability_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
