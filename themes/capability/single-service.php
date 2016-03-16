dcb11a96c7848b72dcb9306d3c42c7cc1df7b041
Jordan <jordan@millenniumgd.com>
2016-03-16 09:36:56 -0400

    Fixed single service header

--- themes/capability/single-service.php
+++ themes/capability/single-service.php
@@ -15,7 +15,7 @@ get_header(); ?>
             $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
             ?>
             <div class="service-theme" style="background-color: <?php the_field('category_color') ?>">
-                <div class="service-container" style="background:url(<?php echo $image[0]; ?>)">
+                <div class="service-container" style="background:url(<?php echo $image[0];?>); background-size:cover; background-position: center;">
                     <div class="service-title">
                         <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
                     </div>
